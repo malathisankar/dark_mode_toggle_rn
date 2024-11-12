c6035ee656a4ecb25fe67772ae47e508622167b4
malathi <malathisankar17@gmail.com>
2024-11-12 22:29:56 +0530

    dark_mode_toggle

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Switch, StyleSheet } from 'react-native';
+import { PaperProvider, Appbar, Text } from 'react-native-paper';
+import { MD3DarkTheme as darkTheme, MD3LightTheme as lightTheme } from 'react-native-paper';
 
-export default function App() {
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Toggle theme between dark and light
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <SafeAreaView style={styles.container}>
+  
+        <Appbar.Header>
+          <Appbar.Content title="Dark Mode Toggle" />
+        </Appbar.Header>
+
+        <Text style={styles.toggleText}>Toggle Dark Mode:</Text>
+        <Switch
+          value={isDarkMode}
+          onValueChange={toggleTheme}
+          style={styles.switch}
+        />
+
+       
+        <Text style={styles.helloText}>Hello, World!</Text>
+      </SafeAreaView>
+    </PaperProvider>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 20,
+  },
+  toggleText: {
+    marginTop: 20,
+    paddingLeft: 20,
+    fontSize: 18,
+  },
+  switch: {
+    marginTop: 20,
+    alignSelf: 'center',
+  },
+  helloText: {
+    marginTop: 50,
+    paddingLeft: 20,
+    fontSize: 22,
   },
 });
+
+export default App;
